e7a68fe65fa9865474b79aebf81774f878a138a6
Jarrod Roberson <jarrod@vertigrated.com>
2009-11-29 23:16:22 -0500

    refactored subscription lists usage to sets module to automatically handle deduping the data. moved to use the kernel/src/inet_dns.hrl header file instead of duplicating it.

--- src/inet_mdns.erl
+++ src/inet_mdns.erl
@@ -1,9 +1,14 @@
 -module(inet_mdns).
 
--include("inet_mdns.hrl").
+-include_lib("kernel/src/inet_dns.hrl").
 
 -export([open/2,start/0]).
 -export([stop/1,receiver/1]).
+
+get_timestamp() ->
+    {Mega,Sec,Micro} = erlang:now(),
+    (Mega*1000000 + Sec)*1000000 + Micro.
+    
 
 open(Addr,Port) ->
    {ok,S} = gen_udp:open(Port,[{reuseaddr,true},{ip,Addr},{multicast_ttl,4},{broadcast,true}, binary]),
@@ -14,7 +19,7 @@ close(S) -> gen_udp:close(S).
 
 start() ->
    S=open({224,0,0,251},5353),
-   Pid=spawn(?MODULE,receiver,[dict:store("_see._tcp.local",[],dict:new())]),
+   Pid=spawn(?MODULE,receiver,[dict:store("_presence._tcp.local",sets:new(),dict:new())]),
    gen_udp:controlling_process(S,Pid),
    {S,Pid}.
 
@@ -34,17 +39,23 @@ receiver(Sub) ->
            receiver(Sub)
    end.
 
-process_dnsrec(_Sub,{error,E}) -> io:format("Error: ~p~n", [E]);
-process_dnsrec(Sub,{ok,#dns_rec{anlist=Responses}}) -> process_dnsrec1(Sub,Responses).
+% process the dns resource records list
+process_dnsrec(_Sub,{error,E}) -> 
+    io:format("Error: ~p~n", [E]);
+process_dnsrec(Sub,{ok,#dns_rec{anlist=Responses}}) -> 
+    process_dnsrec1(Sub,Responses).
 
+% process the list of resource records one at a time
 process_dnsrec1(_,[]) -> ok;
 process_dnsrec1(Sub,[Response|Rest]) ->
   Dom = Response#dns_rr.domain,
   case dict:is_key(Dom,Sub) of
 	  true ->
-		  NewSub = dict:append(Dom, Response, dict:new()),
-		  {ok,Value} = dict:find(Dom, NewSub),
-          io:format("~p=~p~n",[Dom,Value]);
+		  {ok,Value} = dict:find(Dom,Sub),
+          NewRR = Response#dns_rr{tm=get_timestamp()},
+          NewValue = sets:add_element(NewRR,Value),
+          NewSub = dict:store(Dom,NewValue,Sub),
+          io:format("~p=~p~n",[Dom,dict:find(Dom,NewSub)]);
      false ->
           %% do nothing for non-subscribed domains
          ok
