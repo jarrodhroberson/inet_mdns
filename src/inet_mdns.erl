697dbab62c757cb97197689bb259e58f5a2df0aa
Jarrod Roberson <jarrod@vertigrated.com>
2009-11-30 01:09:05 -0500

    I have caching of responses for subscribed queries working!

--- src/inet_mdns.erl
+++ src/inet_mdns.erl
@@ -5,10 +5,10 @@
 -export([open/2,start/0]).
 -export([stop/1,receiver/1]).
 
+% gets a timestamp in ms from the epoch
 get_timestamp() ->
     {Mega,Sec,Micro} = erlang:now(),
-    (Mega*1000000 + Sec)*1000000 + Micro.
-    
+    (Mega*1000000+Sec)*1000000+Micro.
 
 open(Addr,Port) ->
    {ok,S} = gen_udp:open(Port,[{reuseaddr,true},{ip,Addr},{multicast_ttl,4},{broadcast,true}, binary]),
@@ -19,6 +19,7 @@ close(S) -> gen_udp:close(S).
 
 start() ->
    S=open({224,0,0,251},5353),
+   % TODO: this is just for testing, I am adding a subscription for iChat for testing 
    Pid=spawn(?MODULE,receiver,[dict:store("_presence._tcp.local",sets:new(),dict:new())]),
    gen_udp:controlling_process(S,Pid),
    {S,Pid}.
@@ -30,9 +31,14 @@ stop({S,Pid}) ->
 receiver(Sub) ->
   receive
       {udp, _Socket, _IP, _InPortNo, Packet} ->
-          process_dnsrec(Sub,inet_dns:decode(Packet)),
+          DNSREC = inet_dns:decode(Packet),
+          %io:format("******~n~p~n******~n",[DNSREC]),
+          NewSub = process_dnsrec(Sub,DNSREC),
+          receiver(NewSub);
+      {sub,dump} ->
+          io:format("~p~n",[Sub]),
           receiver(Sub);
-       stop -> 
+      stop -> 
 		   true;
        AnythingElse -> 
 		   io:format("RECEIVED: ~p~n",[AnythingElse]),
@@ -45,22 +51,33 @@ process_dnsrec(_Sub,{error,E}) ->
 process_dnsrec(Sub,{ok,#dns_rec{anlist=Responses}}) -> 
     process_dnsrec1(Sub,Responses).
 
+% test to see if a dns_rr.domain is subscribed to
+is_subscribed(_,[]) -> false;
+is_subscribed(Dom,[S|Rest]) ->
+    case lists:suffix(S,Dom) of
+        true ->
+            {ok,S};
+        false ->
+            is_subscribed(Dom,Rest)
+    end.
+
 % process the list of resource records one at a time
-process_dnsrec1(_,[]) -> ok;
+process_dnsrec1(Sub,[]) -> 
+    io:format("Subscriptions: ~p~n",[Sub]),
+    Sub;
 process_dnsrec1(Sub,[Response|Rest]) ->
   Dom = Response#dns_rr.domain,
-  case dict:is_key(Dom,Sub) of
-	  true ->
-		  {ok,Value} = dict:find(Dom,Sub),
+  case is_subscribed(Dom,dict:fetch_keys(Sub)) of
+	  {ok,SD} ->
+		  {ok,Value} = dict:find(SD,Sub),
+          % update the dns_rr to the current timestamp 
           NewRR = Response#dns_rr{tm=get_timestamp()},
           NewValue = sets:add_element(NewRR,Value),
-          NewSub = dict:store(Dom,NewValue,Sub),
-          io:format("~p=~p~n",[Dom,NewValue]),
+          NewSub = dict:store(SD,NewValue,Sub),
+          io:format("Stored: ~p=~p~n",[Dom,NewSub]),
           process_dnsrec1(NewSub,Rest);
      false ->
-          %% do nothing for non-subscribed domains
-         ok
-  end,
-  process_dnsrec1(Sub,Rest).
+          process_dnsrec1(Sub,Rest)
+  end.
 		
 		   
